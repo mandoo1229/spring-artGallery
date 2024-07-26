d791d72bd0e21b32aa09313bfedb2a866e3e0fad
mandoo1229 <skaalsdn35@gmail.com>
2024-07-26 09:48:15 +0900

    Feat : DataBase HikariCp Config 설정

---
+++ src/main/java/kr/co/thereal/artgallery/global/config/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package kr.co.thereal.artgallery.global.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig());
+        return new HikariDataSource(hikariConfig());
+    }
+
+}
