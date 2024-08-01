73752516674d8bd8a13435b129c47900096a800b
mandoo1229 <skaalsdn35@gmail.com>
2024-08-01 16:57:37 +0900

    Feat : 계정 권한 JPA Repository 생성

---
+++ src/main/java/kr/co/thereal/artgallery/domain/admin/repository/AdminRoleRepository.java
@@ -0,0 +1,8 @@
+package kr.co.thereal.artgallery.domain.admin.repository;
+
+
+import kr.co.thereal.artgallery.domain.admin.entity.AdminRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRoleRepository extends JpaRepository<AdminRole, Long> {
+}
