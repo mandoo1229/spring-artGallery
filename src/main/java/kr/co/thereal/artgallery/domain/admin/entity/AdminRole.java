a12c7b0f7bd32c987582ff283dadba84e6ee5632
mandoo1229 <skaalsdn35@gmail.com>
2024-08-01 16:57:16 +0900

    Feat : 계정 권한 DB생성

---
+++ src/main/java/kr/co/thereal/artgallery/domain/admin/entity/AdminRole.java
@@ -0,0 +1,33 @@
+package kr.co.thereal.artgallery.domain.admin.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+public class AdminRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name = "id"))
+    private AdminEntity adminEntity;
+
+    @Builder
+    private AdminRole(Long id, Role role, AdminEntity adminEntity) {
+        this.id = id;
+        this.role = role;
+        this.adminEntity =adminEntity;
+    }
+}
